eb9390b9d08e87df4f5a6b737f84e20584ce2359
aokishun <aosk1919@gmail.com>
2018-11-09 17:54:08 +0900

    ajax1

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -215,7 +215,45 @@ $(document).ready(function() {
 //end
 
 
-// メニューチェンジ
+// ajax使用
+$(function(){
+	//ページを表示させる箇所の設定
+	var $content = $('.pageDisplay');
+	//ボタンをクリックした時の処理
+	$(document).on('click', '.gnavi a', function(event) {
+		event.preventDefault();
+		//.gnavi aのhrefにあるリンク先を保存
+		var link = $(this).attr("href");
+		//リンク先が今と同じであれば遷移しない
+		if(link == lastpage){
+			return false;
+		}else{
+			$content.fadeOut(600, function() {
+				getPage(link);
+			});
+			//今のリンク先を保存
+			lastpage = link;
+		}
 
+	});
+	//初期設定
+	getPage("top.html.erb");
+	var lastpage = "top.html.erb";
+
+	//ページを取得してくる
+    function getPage(elm){
+    	$.ajax({
+            type: 'GET',
+            url: elm,
+            dataType: 'html.erb',
+            success: function(data){
+                $content.html(data).fadeIn(600);
+            },
+            error:function() {
+                       alert('問題がありました。');
+                   }
+    	});
+    }
+});
 
 }());
