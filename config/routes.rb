6fd403f70ffa1f00205ce271110f8d1788289eff
aokishun <aosk1919@gmail.com>
2018-11-11 04:08:51 +0900

    サイドバーの下線訂正

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,5 @@ Rails.application.routes.draw do
   get 'contact'  => 'home#contact'
   get "top" => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'greeting_j' => 'home#greeting_j'
-  get 'event_j'    => 'home#event_j'
-  get 'contact_j'  => 'home#contact_j'
-  get "top_j" => 'home#top_j'
 
 end
