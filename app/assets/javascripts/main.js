3665ad49d9b52750e4237944a6cd0f372f802d96
aokishun <aosk1919@gmail.com>
2018-11-11 03:09:55 +0900

    ajax２回読み込んでしまう

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -215,44 +215,6 @@ $(document).ready(function() {
 //end
 
 
-// ajax_test
-	//ページを表示させる箇所の設定
-	var $content = $('.pageDisplay');
-	//ボタンをクリックした時の処理
-	$(document).on('click', '.gnavi a', function(event) {
-		event.preventDefault();
-		//.gnavi aのhrefにあるリンク先を保存
-		var link = $(this).attr("href");
-		//リンク先が今と同じであれば遷移しない
-		if(link == lastpage){
-			return false;
-		}else{
-			$content.fadeOut(600, function() {
-				getPage(link);
-			});
-			//今のリンク先を保存
-			lastpage = link;
-		}
-
-	});
-	//初期設定
-	getPage("top.html");
-	var lastpage = "top.html";
-
-	//ページを取得してくる
-    function getPage(elm){
-    	$.ajax({
-            type: 'GET',
-            url: elm,
-            dataType: 'html',
-            success: function(data){
-                $content.html(data).fadeIn(600);
-            },
-            error:function() {
-                       alert('問題がありました。');
-                   }
-    	});
-    }
 
 
 }());
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get 'greeting' => 'home#greeting'
   get 'event'    => 'home#event'
   get 'contact'  => 'home#contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "top" => 'home#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'greeting_j' => 'home#greeting_j'
+  get 'event_j'    => 'home#event_j'
+  get 'contact_j'  => 'home#contact_j'
+  get "top_j" => 'home#top_j'
+
 end
